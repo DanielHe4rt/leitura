ce9a992b6a4d574e71037e793b5d0e33929e1df1
danielhe4rt <hey@danielheart.dev>
2022-06-09 17:45:54 -0300

    wip

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,52 +1,48 @@
-@extends('layouts.app2')
+<x-guest-layout>
+    <x-jet-authentication-card>
+        <x-slot name="logo">
+            <x-jet-authentication-card-logo />
+        </x-slot>
 
-@section('content')
-    <div class="row justify-content-center mt-5">
-        <div class="col-6">
-            <div class="card">
-                <div class="card-header bg-primary text-white text-center"> FAÇA SEU LOGIN</div>
-                <div class="card-body">
-                    <div class="row justify-content-center">
-                        <div class="col-12 col-md-9">
+        <x-jet-validation-errors class="mb-4" />
 
-                            @if($errors->any())
-                                <div class="alert alert-danger">
-                                    <ul>
-                                        @foreach($errors->all() as $error)
-                                            <li>{{ $error }}</li>
-                                        @endforeach
-                                    </ul>
-                                </div>
-                            @endif
-                            <p class="card-text">
-                                Entre com as suas credenciais
-                            </p>
-                            <form method="POST" action="{{ route('auth.post-login') }}">
-                                @csrf
-                                <div class="form-group mt-2">
-                                    <label for="email">E-mail</label>
-                                    <input type="email" class="form-control" id="email" name="email"
-                                           aria-describedby="emailHelp" placeholder="seu@email.com">
-                                </div>
-                                <div class="form-group mt-2">
-                                    <label for="password">Senha</label>
-                                    <input type="password" class="form-control" id="password" name="password">
-                                </div>
-                                <div class="d-grid gap-2">
-                                    <button type="submit" class="btn btn-primary mt-2 ">
-                                        Entrar
-                                    </button>
-                                </div>
-                            </form>
-
-                            <div class="d-grid text-center mt-2">
-                                <a href="#">Cadastro</a>
-                                <a href="#">Esqueci minha senha</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+        @if (session('status'))
+            <div class="mb-4 font-medium text-sm text-green-600">
+                {{ session('status') }}
             </div>
-        </div>
-    </div>
-@endsection
+        @endif
+
+        <form method="POST" action="{{ route('login') }}">
+            @csrf
+
+            <div>
+                <x-jet-label for="email" value="{{ __('Email') }}" />
+                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+            </div>
+
+            <div class="mt-4">
+                <x-jet-label for="password" value="{{ __('Password') }}" />
+                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+            </div>
+
+            <div class="block mt-4">
+                <label for="remember_me" class="flex items-center">
+                    <x-jet-checkbox id="remember_me" name="remember" />
+                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                </label>
+            </div>
+
+            <div class="flex items-center justify-end mt-4">
+                @if (Route::has('password.request'))
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+                        {{ __('Forgot your password?') }}
+                    </a>
+                @endif
+
+                <x-jet-button class="ml-4">
+                    {{ __('Log in') }}
+                </x-jet-button>
+            </div>
+        </form>
+    </x-jet-authentication-card>
+</x-guest-layout>
--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [AuthController::class, 'viewLogin'])->name('login');
-Route::post('/login', [AuthController::class, 'postLogin'])->name('auth.post-login');
+Route::get('/', [AuthController::class, 'viewLogin'])->name('landing');
+//Route::post('/login', [AuthController::class, 'postLogin'])->name('auth.post-login');
 Route::get('/logout', [AuthController::class, 'getLogout'])->name('logout');
 
 
